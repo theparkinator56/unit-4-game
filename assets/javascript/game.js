430aed5ce01b72e2f1507e67e68ac98a53face22
TheFreeMan <theparkinator56@gmail.com>
2018-09-26 21:54:15 -0500

    Final solution

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,14 @@
+
 var targetNumber = Math.floor((Math.random() * 129) + 19);
 console.log(targetNumber);
 
-$("#target").html(targetNumber);
+
 
 var counter = 0;
+var wins = 0;
+var losses= 0;
+$("#wins").text(wins);
+$("#losses").text(losses);
 
 
   var numberOptions = [Math.floor((Math.random() * 12) + 1), Math.floor((Math.random() * 12) + 1), Math.floor((Math.random() * 12) + 1), Math.floor((Math.random() * 12) + 1)];
@@ -17,24 +22,33 @@ var counter = 0;
         imageCrystal.attr("src", "assets/images/crystal.jpg");
 
         imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-        $("#minerals").html(imageCrystal);
+        $("#minerals").append(imageCrystal);
         console.log("success");
   }
 
-  $("#crystal-image").on("click", function() {
+  $(".crystal-image").on("click", function() {
       var crystalValue = ($(this).attr("data-crystalvalue"));
       crystalValue = parseInt(crystalValue);
       counter += crystalValue;
-      $("#score").append("New score: " + counter);
+      $("#score").text(counter);
+      $("#target").text(targetNumber);
       
       if (counter === targetNumber) {
             alert("You win!");
-            let numberOptions=(numberOptions)
-            let 
+            counter= 0;
+            numberOptions=(numberOptions);
+            targetNumber=(targetNumber);
+            wins=(wins + 1);
+
           }
       
           else if (counter >= targetNumber) {
             alert("You lose!!");
+            counter= 0;
+            numberOptions=(numberOptions);
+            targetNumber=(targetNumber);
+            losses= (losses+ 1);
+            
           }
       
         });
